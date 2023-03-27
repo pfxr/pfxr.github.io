931ab08089de5f682183e45f2e9273d7a80d5c52
Pedro Rodrigues <pedro.pfxr@gmail.com>
2023-03-27 19:57:02 +0100

    Reacting to the search input better and fetching circle flags right

--- weather-app/index.js
+++ weather-app/index.js
@@ -19,46 +19,62 @@ function handleKeyPress(e){
     // Clear previous selection if any
     location_select.innerHTML="";
     
-    // If enter key
-     if (key==13){
+    // Reset not found
+    not_found.style.display="none";
 
+    // If enter key
+    if (key == 13) {
+
+        // Fetch the city from open-meteo
         fetch("https://geocoding-api.open-meteo.com/v1/search?name="+city.value)
         .then((response) => response.json())
         .then((data) => {
-            for(var i in data.results) {
-                var a = document.createElement('a');
-                var small = document.createElement('small');
-                var li = document.createElement('li');
-                var img = document.createElement('img');
-                console.log(data.results[i]);
+            console.log(data);
+
+            
+            if (!data.results) {
+                // React to the fact that there might not be results
+                console.log("Is undefined");
+                not_found.style.display="block";
+            } else {
+                // Go through the fetch results
+                for(var i in data.results) {
+                    // Create elements to store in <ul> list
+                    var a = document.createElement('a');
+                    var small = document.createElement('small');
+                    var li = document.createElement('li');
+                    var img = document.createElement('img');
+
+
+                    //console.log(data.results[i]);
                 
-                img.src="https://hatscripts.github.io/circle-flags/flags/"+data.results[i].country_code.toLowerCase()+".svg";
-                img.height=24;
-                img.textContent = data.results[i].name;
-                img.title = data.results[i].country;
+                    // Get circle flag 
+                    img.src="https://hatscripts.github.io/circle-flags/flags/"+data.results[i].country_code.toLowerCase()+".svg";
+                    img.height=24;
+                    img.textContent = data.results[i].name;
+                    img.title = data.results[i].country;
 
-                small.className="text-muted"
-                if("admin1" in data.results[i]) {
-                    var details = document.createTextNode(" "+data.results[i].admin1);
-                } else {
-                    var details = document.createTextNode(" "+data.results[i].country);
-                }
+                    small.className="text-muted"
+                    if("admin1" in data.results[i]) {
+                        var details = document.createTextNode(" "+data.results[i].admin1);
+                    } else {
+                        var details = document.createTextNode(" "+data.results[i].country);
+                    }
 
-                small.appendChild(details);
+                    small.appendChild(details);
 
-                a.className="dropdown-item"
-                a.href="#"
-                a.appendChild(img);
-                a.appendChild(document.createTextNode(" "+data.results[i].name));
-                a.appendChild(small);
+                    a.className="dropdown-item"
+                    a.href="#"
+                    a.appendChild(img);
+                    a.appendChild(document.createTextNode(" "+data.results[i].name));
+                    a.appendChild(small);
 
-                li.append(a);
-                location_select.appendChild(li);
+                    li.append(a);
+                    location_select.appendChild(li);
          
-                location_select.style.display="block";               
-            }   
-            location_img.src="https://assets.open-meteo.com/images/country-flags/"+data.results[0].country_code.toLowerCase()+".svg";         
-
+                    location_select.style.display="block";               
+                }   
+            }
         });
         //fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
         //.then((response) => response.json())
