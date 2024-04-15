31d01e9d021ba85cfaf183adde3e0fee4698cfac
Pedro Rodrigues <pedro.pfxr@gmail.com>
2024-04-15 21:39:17 +0100

    Adding new UI

--- xiaomi_temperature_sensor/index.js
+++ xiaomi_temperature_sensor/index.js
@@ -1,16 +1,27 @@
+const temp_label = document.getElementById('temp');
+const temp_label_c = document.getElementById('tempC');
+const humid_label = document.getElementById('humid');
+const bat_label = document.getElementById('bat');
+const connect_btn_div = document.getElementById('btn_div');
+const connect_button = document.getElementById('connectButton');
+const loader = document.getElementById('loader')
+
+
 // The remote service we wish to connect to.
 const TEMPERATURE_SERVICE_UUID  = "ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6"
 const TEMPERATURE_CHAR_UUID = "ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const connectButton = document.getElementById('connectButton');
+var connect_loader = false;
 
-  connectButton.addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
+
+  connect_button.addEventListener('click', function () {
     connectToDevice();
   });
 });
 
 async function connectToDevice() {
+    loader.removeAttribute('hidden');
     try {
       connected_device = await getDevice(["LYW"], [
         TEMPERATURE_SERVICE_UUID,
@@ -23,25 +34,18 @@ async function connectToDevice() {
 };
 
 function handle_notifications(event) {
+  connect_btn_div.setAttribute("hidden", true);
   let value = event.target.value;
   let a = [];
   // Convert raw data bytes to hex values just for the sake of showing something.
   // In the "real" world, you'd use data.getUint8, data.getUint16 or even
   // TextDecoder to process raw data bytes.
   temp = (value.getUint8(0) | value.getUint8(1)<<8) * 0.01;
-  console.log(temp);
+  temp_label.textContent = temp.toFixed(1)
+  temp_label_c.textContent = "ºC"
+  humid_label.textContent = "^_^"
+  bat_label.textContent = "60"
 }
-
-//function handle_notifications(event) {
-//  let value = event.target.value;
-//  console.log(value)
-//
-//  let temp = (value.getInt32(0, true));// | (value.getInt32(1, true) << 8)) * 0.01; //little endian
-//  let humi = value[2];
-//  let voltage = (value[3] | (value[4] << 8)) * 0.001; //little endian
-//
-//  console.log(temp);
-//}
 
 async function connectAndSubscribeToCharacteristics(device) {
   subscribe_characteristic(device, TEMPERATURE_SERVICE_UUID, TEMPERATURE_CHAR_UUID, handle_notifications);
@@ -143,141 +147,8 @@ function subscribe_characteristic(device, serviceUUID, characteristicUUID, func)
   }
 }
 
-function unsubscribe_characteristic(device, serviceUUID, characteristicUUID, func) {
-  try {
-    device.gatt.getPrimaryService(serviceUUID).then(service => { // use the connected GATT server
-      return service.getCharacteristics();
-    }).then(
-      characteristics => {
-        characteristics.forEach(characteristic => {
-          if (characteristic.uuid === characteristicUUID) {
-            console.log('Found the characteristic');
-
-            var myCharacteristic = characteristic;
-            myCharacteristic.removeEventListener('characteristicvaluechanged', func);
-            characteristic.stopNotifications().then(_ => {
-              console.log('> Notifications stopped');
-            });
-          } else {
-            console.log('characteristic ' + characteristic.uuid + ' in svc  ' + serviceUUID)
-          }
-        })
-      }
-    )
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-function write_characteristic(device, serviceUUID, characteristicUUID, cmd) {
-  const textEncoder = new TextEncoder();
-  console.log("writing " + cmd)
-  try {
-    device.getPrimaryService(serviceUUID).then(service => {
-      return service.getCharacteristics();
-    }).then(
-      characteristics => {
-        characteristics.forEach(characteristic => {
-          if (characteristic.uuid === characteristicUUID) {
-            characteristic.writeValueWithoutResponse(textEncoder.encode(cmd));
-          }
-        })
-      }
-    )
-  } catch (error) {
-    console.log(error)
-  }
-
-}
-
-function readAppearanceValue(characteristic) {
-  return characteristic.readValue().then(value => {
-    console.log('> Appearance: ' +
-      getDeviceType(value.getUint16(0, true /* Little Endian */)));
-  });
-}
-
-function readDeviceNameValue(characteristic) {
-  return characteristic.readValue().then(value => {
-    console.log('> Device Name: ' + new TextDecoder().decode(value));
-  });
-}
-
-function readPPCPValue(characteristic) {
-  return characteristic.readValue().then(value => {
-    console.log('> Peripheral Preferred Connection Parameters: ');
-    console.log('  > Minimum Connection Interval: ' +
-      (value.getUint8(0) | value.getUint8(1) << 8) * 1.25 + 'ms');
-    console.log('  > Maximum Connection Interval: ' +
-      (value.getUint8(2) | value.getUint8(3) << 8) * 1.25 + 'ms');
-    console.log('  > Latency: ' +
-      (value.getUint8(4) | value.getUint8(5) << 8) + 'ms');
-    console.log('  > Connection Supervision Timeout Multiplier: ' +
-      (value.getUint8(6) | value.getUint8(7) << 8));
-  });
-}
-
-function readCentralAddressResolutionSupportValue(characteristic) {
-  return characteristic.readValue().then(value => {
-    let supported = value.getUint8(0);
-    if (supported === 0) {
-      console.log('> Central Address Resolution: Not Supported');
-    } else if (supported === 1) {
-      console.log('> Central Address Resolution: Supported');
-    } else {
-      console.log('> Central Address Resolution: N/A');
-    }
-  });
-}
-
-function readPeripheralPrivacyFlagValue(characteristic) {
-  return characteristic.readValue().then(value => {
-    let flag = value.getUint8(0);
-    if (flag === 1) {
-      console.log('> Peripheral Privacy Flag: Enabled');
-    } else {
-      console.log('> Peripheral Privacy Flag: Disabled');
-    }
-  });
-}
-
-/* Utils */
-
 function getDeviceType(value) {
   // Check out page source to see what valueToDeviceType object is.
   return value +
     (value in valueToDeviceType ? ' (' + valueToDeviceType[value] + ')' : '');
-}
-
-function populateBluetoothDevices(supported_prefixes) {
-  try {
-    const devicesSelect = document.querySelector('#devicesSelect');
-    console.log('Getting existing permitted Bluetooth devices...');
-    navigator.bluetooth.getDevices()
-      .then(devices => {
-        console.log('> Got ' + devices.length + ' Bluetooth devices.');
-        devicesSelect.textContent = '';
-        for (const device of devices) {
-
-          //check if the device names starts with the supported prefix
-          if (!supported_prefixes.some(prefix => device.name.startsWith(prefix))) {
-            continue;
-          }
-
-          const option = document.createElement('option');
-          option.value = device.id;
-          option.textContent = device.name;
-          devicesSelect.appendChild(option);
-        }
-      })
-      .catch(error => {
-        console.log('Argh! ' + error);
-      });
-  }
-  catch {
-    //not supported
-    console.log("Not supported")
-  }
-
 }
